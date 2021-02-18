c43fb184c283c43ecab22005ae99cfa3b32b4a5e
Kanersps <kanepetra@gmail.com>
2021-02-18 18:55:09 +0100

    tweak: remove checkbox component as we're not using it

--- src/Account/Login.js
+++ src/Account/Login.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import Title from "antd/es/typography/Title";
 import {Link, Redirect} from "react-router-dom";
